8044128e967a1e8f04bc587156965e5dcf580db7
ZoraCode <142979733+ZoraCode@users.noreply.github.com>
2024-03-29 14:21:46 +0900

    save

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
 
 
-
-
 let tabList = document.querySelectorAll('.sec2 .tab_nav .tab li'),
     tabs = document.querySelectorAll('.sec2 .tab_ul > li'),
     tabListb = document.querySelectorAll('.sec6 .tab_nav .tab li'),
@@ -30,5 +28,38 @@ tabListb.forEach((tem,idx)=>{
 
   });
 });
+
+
+
+// 모든 .tab_ul li span 요소 선택
+let btnClick = document.querySelectorAll('.tab_ul li span');
+
+// 각 요소에 대해 이벤트 리스너 추가
+btnClick.forEach(item => {
+    // 클릭 이벤트 처리
+    item.addEventListener('click', () => {
+        // 부모 요소인 li 요소 찾기
+        let li = item.parentNode;
+        // block 클래스가 있는 요소인지 확인
+        let BlockClass = li.querySelector('.block');
+        // none 클래스가 있는 요소인지 확인
+        let NoneClass = li.querySelector('.none');
+
+        // block 클래스가 있을 경우, none 클래스를 block으로, block 클래스를 none으로 변경
+        if (BlockClass) {
+            BlockClass.classList.remove('block');
+            BlockClass.classList.add('none');
+        }
+        if (NoneClass) {
+            NoneClass.classList.remove('none');
+            NoneClass.classList.add('block');
+        }
+    });
+});
+
+
+
+
+
 
 });
